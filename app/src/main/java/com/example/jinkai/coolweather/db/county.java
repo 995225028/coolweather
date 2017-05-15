2d509aa3ba0d0e8ba328e8a6e4609a2767090416
jinkai <995225028@qq.com>
2017-05-15 17:48:21 +0800

    create db

---
+++ app/src/main/java/com/example/jinkai/coolweather/db/county.java
@@ -0,0 +1,46 @@
+package com.example.jinkai.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jinkai on 2017/5/15.
+ */
+
+public class county extends DataSupport {
+    private int id;
+    private String countyName;
+    private  String weatherID;
+    private int cityID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(String weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(int cityID) {
+        this.cityID = cityID;
+    }
+}
