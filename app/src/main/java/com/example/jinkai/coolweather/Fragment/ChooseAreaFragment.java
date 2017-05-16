0424ae2d5bfebe45c86a294ee8d7056cab093897
jinkai <995225028@qq.com>
2017-05-16 11:11:03 +0800

    DbUtil

--- app/src/main/java/com/example/jinkai/coolweather/Fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/example/jinkai/coolweather/Fragment/ChooseAreaFragment.java
@@ -1,5 +1,6 @@
 package com.example.jinkai.coolweather.Fragment;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,11 +12,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jinkai.coolweather.R;
+import com.example.jinkai.coolweather.utils.HttpUtil;
+import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * Created by jinkai on 2017/5/15.
@@ -32,6 +41,7 @@ public class ChooseAreaFragment extends Fragment implements View.OnClickListener
     private ArrayAdapter<String> stringArrayAdapter;
     private List<String> datalist=new ArrayList<>();
     private int currentLevel;
+    private ProgressDialog progressDialog;
 
     public static ChooseAreaFragment newInstance() {
 
@@ -110,6 +120,48 @@ public class ChooseAreaFragment extends Fragment implements View.OnClickListener
      * @param type
      */
     private void queryFromServer(String address,final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //通过runOnUiThread()方法回到主线程处理逻辑
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
 
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseStr = response.body().string();
+                if ("province".equals(type)){
+
+                }
+            }
+        });
+    }
+
+
+    /**
+     * 显示进度对话框
+     */
+    private void showProgressDialog(){
+        if (progressDialog==null){
+            progressDialog=new ProgressDialog(getContext());
+            progressDialog.setMessage("正在加载...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+    /**
+     * 关闭进度对话框
+     */
+    private void closeProgressDialog(){
+        if (progressDialog!=null){
+            progressDialog.dismiss();
+        }
     }
 }
---
+++ app/src/main/java/com/example/jinkai/coolweather/utils/DbUtil.java
@@ -0,0 +1,95 @@
+package com.example.jinkai.coolweather.utils;
+
+import android.text.TextUtils;
+
+import com.example.jinkai.coolweather.db.City;
+import com.example.jinkai.coolweather.db.County;
+import com.example.jinkai.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+/**
+ * Created by jinkai on 2017/5/16.
+ */
+
+public class DbUtil {
+    /**
+     * 解析和处理服务器返回的省级数据
+     * @param response
+     * @return
+     */
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i <allProvinces.length() ; i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的市级数据
+     * @param response
+     * @param provinceID
+     * @return
+     */
+    public static boolean handleCityResponse(String response,int provinceID){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i <allCities.length() ; i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setCityName(cityObject.getString("name"));
+                    city.setProvinceID(provinceID);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的县级数据
+     * @param response
+     * @param cityID
+     * @return
+     */
+    public static boolean handleCountyResponse(String response,int cityID){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray counties = new JSONArray(response);
+                for (int i = 0; i < counties.length(); i++) {
+                    JSONObject countyObject = counties.getJSONObject(i);
+                    County county = new County();
+                    county.setCityID(cityID);
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherID(countyObject.getString("weather_id"));
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
